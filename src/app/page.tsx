d9f07af5381ca490e471736af0e5f354bbce3b64
Noor_Mohammad <noor.jsdivs@gmail.com>
2024-03-02 01:36:08 +0600

    hero sections done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Pricing from "@/components/Pricing";
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <main className="py-20 bg-gray-900 text-gray-200">
-      <Pricing />
+    <main>
+      <Hero />
     </main>
   );
 }
